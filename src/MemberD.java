40cf3b18accfeadf9c4dfe8b9997fb36908575fa
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-06-07 15:25:52 +0200

    a7

--- src/MemberD.java
+++ src/MemberD.java
@@ -1,7 +1,7 @@
 public class MemberD {
 
   public void future(String message) {
-    System.out.println("Your future looks like " + message);
+    System.out.println("Your future looks like this:" + message);
   }
 }
 
