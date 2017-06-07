2083c3de54b5500d8e59b107274c9002d148d3f0
Balazs Stegmar <stegmarb@gmail.com>
2017-06-07 13:57:02 +0200

    d1

--- src/GitPro.java
+++ src/GitPro.java
@@ -3,5 +3,10 @@ public class GitPro {
 
   public static void main(String[] args) {
     System.out.println("Hello Kryptonite!");
+    future();
+  }
+
+  public static void future() {
+    System.out.println("Cancer");
   }
 }
